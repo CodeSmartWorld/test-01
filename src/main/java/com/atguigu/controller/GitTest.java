236ccb37b137d4788b5e93f0ceee0f228c44527c
fengyq <fengyq@qq.com>
2017-11-23 14:24:19 +0800

    test01 conflic

--- src/main/java/com/atguigu/controller/GitTest.java
+++ src/main/java/com/atguigu/controller/GitTest.java
@@ -2,7 +2,7 @@ package com.atguigu.controller;
 
 public class GitTest {
 	public void test(){
-		System.out.println("hello GitHub");	
+		System.out.println("hello GitHub");//	[test01 conflic]
 		System.out.println("sya goodbye,say goodbye ...");
 	}
 }
