c6c9d23f506627f81d235713c88e41d23e0046d9
fengyq <fengyq@qq.com>
2017-11-23 14:28:55 +0800

    dsad

--- src/main/java/com/atguigu/controller/GitTest.java
+++ src/main/java/com/atguigu/controller/GitTest.java
@@ -3,6 +3,6 @@ package com.atguigu.controller;
 public class GitTest {
 	public void test(){
 		System.out.println("hello GitHub");//	[test01 conflic]
-		System.out.println("sya goodbye,say goodbye ...");
+		System.out.println("sya goodbye,say goodbye ...");//[test 01 conflic 2]
 	}
 }
